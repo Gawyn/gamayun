78a304ff8cb8c9c6edf0980a0b710679011f023b
Cristian Planas <cplanas@zendesk.com>
2021-01-21 23:54:40 -0500

    Loading of controllers

--- lib/gamayun/config.rb
+++
@@ -1,9 +0,0 @@
-require 'yaml'
-
-module Gamayun
-  module Config
-    def config
-      @config ||= YAML.load(File.read("#{Rails.root}/config/gamayun.yml"))
-    end
-  end
-end
--- lib/gamayun/engine.rb
+++ lib/gamayun/engine.rb
@@ -2,8 +2,43 @@ module Gamayun
   class Engine < ::Rails::Engine
     isolate_namespace Gamayun
 
+    initializer "engine_name.assets.precompile" do |app|
+      app.config.assets.precompile += %w( stylesheets/gamayun.css )
+      app.config.assets.precompile += %w( gamayun/application.css )
+    end
+
     def self.app_config
-      @app_config ||= YAML.load(File.read("#{Rails.root}/config/gamayun.yml"))
+      filename = "#{Rails.root}/config/gamayun.yml"
+      return @app_config if @app_config
+      @app_config = File.exist?(filename) ? YAML.load(File.read(filename)) : Hash.new([])
+      Gamayun::Engine.load_controllers!
+      @app_config
+    end
+
+    def self.load_controllers!
+      Engine.app_config['manageable_models'].each do |model_attrs|
+        s = <<-CONTROLLERCLASS
+          require_dependency "gamayun/application_controller"
+          require_dependency "gamayun/admin/base_controller"
+          require_dependency "gamayun/admin/resources_controller"
+
+          class ::Gamayun::Admin::#{model_attrs['model'].capitalize.pluralize}Controller < Gamayun::Admin::ResourcesController
+            before_action :require_role!
+
+            private
+
+            def model_name
+              \"#{model_attrs['model']}\"
+            end
+
+            def require_role!
+              # return false unless current_user.role == #{model_attrs['required_role']}
+            end
+          end
+        CONTROLLERCLASS
+
+        eval(s)
+      end
     end
   end
 end
