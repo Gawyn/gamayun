9588229456afe33c7c175c9de323a89dc403ce87
Cristian Planas <cplanas@zendesk.com>
2021-02-20 11:17:24 -0500

    Setting user on creation

--- app/models/gamayun/user.rb
+++ app/models/gamayun/user.rb
@@ -4,5 +4,15 @@ module Gamayun
     # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
     devise :database_authenticatable, :registerable,
            :recoverable, :rememberable, :validatable
+
+    before_save :set_role!
+
+    def set_role!
+      if self.class.count > 0
+        self.role = 'end_user'
+      else
+        self.role = 'admin'
+      end
+    end
   end
 end
